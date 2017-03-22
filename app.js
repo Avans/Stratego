b508b31ddc0e620d682289491a5807e751231bc4
Paul Wagener <mail@paulwagener.nl>
2017-03-22 09:49:04 +0100

    More documentation

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var YAML = require('yamljs');
 
 module.exports = app;
@@ -10,12 +11,16 @@ var config = {
   appRoot: __dirname
 };
 
+
+
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
   swaggerExpress.register(app);
 
   app.listen(process.env.PORT || 3000);
+
+  app.use('/static', express.static('static'));
 
   var swaggerUi = require('swagger-ui-express');
   app.use('/', swaggerUi.serve, swaggerUi.setup(YAML.load('./api/swagger/swagger.yaml')));
