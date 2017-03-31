f4e6a0550c1e21ddc32024e74385b54454af7049
Paul Wagener <mail@paulwagener.nl>
2017-03-31 10:46:15 +0200

    PieceType.canBeat() implemented

--- models/PieceType.js
+++ models/PieceType.js
@@ -2,13 +2,33 @@ const util = require('util');
 
 module.exports = PieceType;
 
-function PieceType(code, name, number_per_player) {
+function PieceType(code, name, relative_value, number_per_player) {
     this.code = code;
     this.name = name;
+    this.relative_value = relative_value;
     this.number_per_player = number_per_player;
 
     this.toString = function() {
         return util.format("%s ('%s')", name, code);
+    }
+
+    this.canBeat = function(otherPieceType) {
+        // Spy can beat Marshal
+        if(this == PieceType.TYPES.SPY && otherPieceType == PieceType.TYPES.MARSHAL) {
+            return true;
+        }
+
+        // Miner can beat bomb
+        if(this == PieceType.TYPES.MINER && otherPieceType == PieceType.TYPES.BOMB) {
+            return true;
+        }
+
+        // Higher pieces can beat lower pieces
+        if(this.relative_value >= otherPieceType.relative_value) {
+            return true;
+        }
+
+        return false;
     }
 
     Object.freeze(this);
@@ -29,16 +49,16 @@ PieceType.getCodes = function(code) {
 }
 
 PieceType.TYPES = Object.freeze({
-    BOMB: new PieceType('B', 'Bomb', 6),
-    MARSHAL: new PieceType('1', 'Marshal', 1),
-    GENERAL: new PieceType('2', 'General', 1),
-    COLONEL: new PieceType('3', 'Colonel', 2),
-    MAJOR: new PieceType('4', 'Major', 3),
-    CAPTAIN: new PieceType('5', 'Captain', 4),
-    LIEUTENANT: new PieceType('6', 'Lieutenant', 4),
-    SERGEANT: new PieceType('7', 'Sergeant', 4),
-    MINER: new PieceType('8', 'Miner', 5),
-    SCOUT: new PieceType('9', 'Scout', 8),
-    SPY: new PieceType('S', 'Spy', 1),
-    FLAG: new PieceType('F', 'Flag', 1)
+    BOMB: new PieceType('B', 'Bomb', 11, 6),
+    MARSHAL: new PieceType('1', 'Marshal', 10, 1),
+    GENERAL: new PieceType('2', 'General', 9, 1),
+    COLONEL: new PieceType('3', 'Colonel', 8, 2),
+    MAJOR: new PieceType('4', 'Major', 7, 3),
+    CAPTAIN: new PieceType('5', 'Captain', 6, 4),
+    LIEUTENANT: new PieceType('6', 'Lieutenant', 5, 4),
+    SERGEANT: new PieceType('7', 'Sergeant', 4, 4),
+    MINER: new PieceType('8', 'Miner', 3, 5),
+    SCOUT: new PieceType('9', 'Scout', 2, 8),
+    SPY: new PieceType('S', 'Spy', 1, 1),
+    FLAG: new PieceType('F', 'Flag', 0, 1)
 });
---
+++ test/models/piecetype.js
@@ -0,0 +1,39 @@
+const should = require('should');
+
+const PieceType = require('../../models/PieceType');
+
+
+/**
+ * Test the order in which pieces can beat eachother
+ */
+describe('PieceType.canBeat()', function() {
+    it("Colonel should beat Sergeant", async function() {
+        PieceType.TYPES.COLONEL.canBeat(PieceType.TYPES.SERGEANT).should.be.true();
+    });
+
+    it("Marshal should not beat Bomb", async function() {
+        PieceType.TYPES.MARSHAL.canBeat(PieceType.TYPES.BOMB).should.be.false();
+    });
+
+    it("Marshal should beat Spy", async function() {
+        PieceType.TYPES.MARSHAL.canBeat(PieceType.TYPES.SPY).should.be.true();
+    });
+
+    it("Spy should beat Marshal", async function() {
+        PieceType.TYPES.SPY.canBeat(PieceType.TYPES.MARSHAL).should.be.true();
+    });
+
+    it("Miner should beat Bomb", async function() {
+        PieceType.TYPES.MINER.canBeat(PieceType.TYPES.BOMB).should.be.true();
+    });
+
+    it("Scout should beat Scout", async function() {
+        // Equal values can beat the other
+        // with the provision that they are themselves also destroyed (that logic is somewhere else)
+        PieceType.TYPES.SCOUT.canBeat(PieceType.TYPES.SCOUT).should.be.true();
+    });
+
+    it("Spy should beat Flag", async function() {
+        PieceType.TYPES.SPY.canBeat(PieceType.TYPES.FLAG).should.be.true();
+    });
+});
