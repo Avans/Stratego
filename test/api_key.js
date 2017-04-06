c08904018c77158a2a19d2cbc55498355327a3b0
Paul Wagener <mail@paulwagener.nl>
2017-04-06 07:58:54 +0200

    More API key code coverage

--- test/api_key.js
+++ test/api_key.js
@@ -7,9 +7,21 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 /**
- * API key
+ * API key stuff
+ *
+ * Yes, this is mainly just to brag 100% code coverage
  */
-describe('serializeUser()', function() {
+describe('api_key', function() {
+    // Example profile
+    let profile;
+
+    beforeEach(async function() {
+        profile = {
+            nickname: "Test User",
+            id: "test_id"
+        }
+    });
+
     let test_user;
     beforeEach(async function() {
         await User.remove();
@@ -21,11 +33,44 @@ describe('serializeUser()', function() {
         await test_user.save();
     });
 
-    it('should give the ._id property', function(done) {
+    it('should give the ._id property for serializeUser()', function(done) {
         api_key.serializeUser(test_user, function(err, userId) {
             should.not.exist(err);
             userId.should.equal('test_id');
             done();
         })
     });
+
+    it('should find the user for deserializeUser()', function(done) {
+        api_key.deserializeUser('test_id', function(err, user) {
+            should.not.exist(err);
+            user._id.should.equal('test_id');
+            done();
+        });
+    });
+
+    it('should return existing user for Strategy', function(done) {
+        api_key.getUserStrategy('', '', profile, function(err, user) {
+            user._id.should.equal('test_id');
+
+            User.count(function(err, c) {
+                c.should.equal(1);
+                done();
+            });
+        });
+    });
+
+    it('should create new user for Strategy', function(done) {
+        profile.id = 'someone_else';
+        api_key.getUserStrategy('', '', profile, function(err, user) {
+            user._id.should.equal('someone_else');
+
+            User.count(function(err, c) {
+                c.should.equal(2);
+                done();
+            });
+        });
+    });
+
+
 });
